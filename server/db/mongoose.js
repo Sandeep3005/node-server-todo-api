7d13d086644a50c9f45af392925e858df73f04a4
Sandeep Chikhale <chikhale.sandeep.2002@gmail.com>
2017-07-02 14:34:36 +0530

    Testing-2

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-let uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp';
+let remoteURI = 'mongodb://sandeep3005:todosapi@ds147072.mlab.com:47072/node-todos
+'
+let uri = remoteURI;//process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp';
 // mongodb://<dbuser>:<dbpassword>@ds147072.mlab.com:47072/node-todos
 mongoose.connect(uri)
 
